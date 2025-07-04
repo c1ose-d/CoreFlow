4ec60cc8a79b46c4c990336c268771dbf698a5b2
c1ose-d <michael.glodin2004@gmail.com>
2025-07-04 16:42:50 +0300

    v0.0.0.0

---
+++ CoreFlow.Presentation/Common/BaseViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreFlow.Presentation.Common;
+
+public abstract partial class BaseViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool _isBusy;
+
+    [ObservableProperty]
+    private string? _title;
+}
